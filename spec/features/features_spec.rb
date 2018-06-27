7001c58370f877ed77a243772486ee70071b4ed2
John Forster <j_forster@hotmail.co.uk>
2018-06-27 14:49:36 +0100

    Edited feature test to use Feature/Scenario syntax

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,5 +1,5 @@
-describe 'all the features', type: :feature do
-  it 'tests infrastructure working' do
+feature 'testing infrastructure' do
+  scenario 'Can run app and check page content' do
     visit('/')
     expect(page).to have_content('Testing Infrastructure Working')
   end
