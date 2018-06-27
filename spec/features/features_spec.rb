d0c745990305bd891858e7170b8610241cf1ad64
Cornelis Heuperman <cheuperman@gmail.com>
2018-06-27 15:10:30 +0100

    Allow user to enter names of battlers

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -4,3 +4,13 @@ feature 'testing infrastructure' do
     expect(page).to have_content('Testing Infrastructure Working')
   end
 end
+
+feature 'players can enter their names' do
+  scenario 'displays names after entry' do
+    visit('/')
+    fill_in 'player1', with: 'John'
+    fill_in 'player2', with: 'Kees'
+    click_button 'Submit'
+    expect(page).to have_content('John vs. Kees!!!')
+  end
+end
