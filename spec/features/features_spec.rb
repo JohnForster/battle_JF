ea34abe3cdac4eccd6c2f00ec83603e06dd687c6
John Forster <j_forster@hotmail.co.uk>
2018-06-27 15:32:44 +0100

    Done some sweet formatting magic

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -8,9 +8,9 @@ end
 feature 'players can enter their names' do
   scenario 'displays names after entry' do
     visit('/')
-    fill_in 'player1', with: 'John'
-    fill_in 'player2', with: 'Kees'
-    click_button 'Submit'
+    fill_in :player1, with: 'John'
+    fill_in :player2, with: 'Kees'
+    click_button :Submit
     expect(page).to have_content('John vs. Kees!!!')
   end
 end
